952e7b2fcd219a5424adc921150a2f67c5e2b2c7
tyj144 <tylerj81@gmail.com>
2017-12-02 11:49:52 -0500

    Serialized airport data, generalized csvparse

--- csvparse.py
+++ csvparse.py
@@ -1,18 +1,21 @@
 import csv
 from dateutil import parser
 
-flight_list = []
-with open('Lowest_Fares.csv') as f:
-   reader = csv.reader(f)
-   flight_list += list(reader)
-   
-headings = flight_list[0]
-flight_list = flight_list[1:]
+def get_rows(filename, date_field=None):
+	with open(filename) as f:
+		reader = csv.reader(f)
+		csv_list = list(reader)
+	   
+	# get headings
+	headings = csv_list[0]
+	# remove headings from other data
+	csv_list = csv_list[1:]
 
+	# turn the each row into a dictionary
+	rows = [ dict(zip(headings, row)) for row in csv_list ]
 
-flights = [ dict(zip(headings, row)) for row in flight_list ]
+	if date_field:
+		for dictionary in rows:
+			dictionary[date_field] = parser.parse(dictionary[date_field], fuzzy=True)
 
-for dictionary in flights:
-   dictionary["FlightDate"] = parser.parse(dictionary["FlightDate"], fuzzy=True)
-
-print(flights[0:4])
+	return rows
